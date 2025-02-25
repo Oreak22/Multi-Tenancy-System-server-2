17267b581bbb077ac0b2b8eefd88b21ad1045d80
oreak22 <rejoiceobe235@gmail.com>
2025-02-25 15:24:30 +0100

    added password reset function

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { login } = require("../controllers/authController");
+const {
+	login,
+	resetPassword,
+	resetPasswordConfirm,
+} = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/login", login);
+router.post("/resetpassword", resetPassword);
+router.post("/changepassword", resetPasswordConfirm);
 
 module.exports = router;
